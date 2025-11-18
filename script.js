1d6556a09418aea2d657c20393674fd299e52ee0
Omar Nuñez <49326511+choredarck@users.noreply.github.com>
2025-11-18 01:03:47 -0600

    Add script to display current year in footer <181125>

---
+++ script.js
@@ -0,0 +1,7 @@
+// Año en el footer
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
